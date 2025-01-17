fb7f7f72dbebb3f797cd3d1107f2b44c0b877ef5
zhaoshijie <zhao_shjie@163.com>
2025-01-17 21:56:48 +0800

    lightIR API

--- tests/2-ir-gen/warmup/stu_cpp/if_generator.cpp
+++ tests/2-ir-gen/warmup/stu_cpp/if_generator.cpp
@@ -1,1 +1,55 @@
-int main() {}
+#include "BasicBlock.hpp"
+#include "Constant.hpp"
+#include "Function.hpp"
+#include "IRBuilder.hpp"
+#include "Module.hpp"
+#include "Type.hpp"
+
+#include <iostream>
+#include <memory>
+
+// 定义一个从常数值获取/创建 ConstantInt 类实例化的宏，方便多次调用
+#define CONST_INT(num) ConstantInt::get(num, module)
+// 定义一个从常数值获取/创建 ConstantFP 类实例化的宏，方便多次调用
+#define CONST_FP(num) ConstantFP::get(num, module)
+
+int main() {
+    // 创建一个 Module 实例
+    auto module = new Module();
+    // 创建一个 IRBuilder 实例（后续创建指令均使用此实例操作）
+    auto builder = new IRBuilder(nullptr, module);
+    // 从 Module 处取出 32 位整形 type 的实例
+    Type *Int32Type = module->get_int32_type();
+
+
+    auto mainFun = Function::create(FunctionType::get(Int32Type, {}), "main", module);
+
+    auto bb = BasicBlock::create(module, "entry", mainFun);
+    builder->set_insert_point(bb);
+    auto retAlloca = builder->create_alloca(Int32Type);
+    builder->create_store(CONST_INT(0), retAlloca);
+
+    Type *FloatType = module->get_float_type();
+    auto aAlloca = builder->create_alloca(FloatType);
+    builder->create_store(CONST_FP(5.555), aAlloca);
+
+    auto aLoad = builder->create_load(aAlloca);
+    auto fcmp = builder->create_fcmp_gt(aLoad, CONST_FP(1.0));
+
+    auto trueBB = BasicBlock::create(module, "trueBB", mainFun);
+    auto retBB = BasicBlock::create(module, "retBB", mainFun);
+
+    builder->create_cond_br(fcmp, trueBB, retBB);
+
+    builder->set_insert_point(trueBB);
+    builder->create_store(CONST_INT(233), retAlloca);
+    builder->create_br(retBB);
+
+    builder->set_insert_point(retBB);
+    builder->create_ret(builder->create_load(retAlloca));
+
+    std::cout << module->print();
+    
+    delete module;
+    return 0;
+}
